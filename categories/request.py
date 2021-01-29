08d2ce6e3bf5a6ddf876a311a0c03fee29d33573
Susan Wiens <susanmichellewiens@gmail.com>
2021-01-29 13:23:31 -0600

    adding models

--- categories/request.py
+++ categories/request.py
@@ -18,3 +18,37 @@ def create_category(new_category):
         new_category['id'] = id
 
         return json.dumps(new_category)
+
+def get_all_categories():
+    
+    with sqlite3.connect("./rare.db") as conn:
+
+        
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        
+        db_cursor.execute("""
+        SELECT
+            c.id,
+            c.label
+        FROM category c
+        """)
+
+        categories = []
+
+        dataset = db_cursor.fetchall()
+
+        # Iterate list of data returned from database
+        for row in dataset:
+
+            # Create an animal instance from the current row.
+            # Note that the database fields are specified in
+            # exact order of the parameters defined in the
+            # Animal class above.
+            category = Category(row['id'], row['label'])
+
+            categories.append(category.__dict__)
+
+    # Use `json` package to properly serialize list as JSON
+    return json.dumps(categories)
